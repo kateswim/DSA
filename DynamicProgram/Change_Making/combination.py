8fc6ba8b7fd4ab3c94943831562f6df875ec080d
kateswim <goncharovakiev@gmail.com>
2025-05-05 17:14:23 +0300

    updated

--- DynamicProgram/Change_Making/combination.py
+++ DynamicProgram/Change_Making/combination.py
@@ -2,11 +2,25 @@ from functools import lru_cache
 
 def count_ways_to_pay(coins, amount):
     @lru_cache(maxsize=None)
-    def count_ways_helper(amount, index):
+    def count_ways_helper(index, amount):
         if amount == 0:
             return 1
-        if amount < 0 or index < 0:
+        if amount < 0 or index == len(coins):
             return 0
         
+        total_ways = 0
+        coin = coins[index]
+        for repeats in range(amount // coin + 1):
+            payment = coin * repeats
+            total_ways += count_ways_helper(index + 1, amount - payment)
+            print(f"Using coin {coin}, repeats: {repeats}, remaining amount: {amount - payment}, total ways so far: {total_ways}")
+
+        return total_ways
+    return count_ways_helper(0, amount)
+
+coins = [2, 5, 10]
+amount = 17
+print(count_ways_to_pay(coins, amount))  
+
         
         
