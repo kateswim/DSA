51c19bc141b843a7715e68afde63067f645e04ea
kateswim <goncharovakiev@gmail.com>
2025-07-05 23:01:45 -0700

    updates

--- Two Pointers/triplet_sum.py
+++ Two Pointers/triplet_sum.py
@@ -2,4 +2,30 @@ def triplet_sum(list):
     list.sort()
     triplets = []
     for i in range(len(list)):
+        if list[i] > 0: # means that if first number positive (> 0) others also positive in sorted list
+            break
+        if i > 0 and list[i] == list[i-1]: # current number is same as previous
+            continue
+        pairs = pair_sum_sorted(list, i+1, -list[i])
+
+        for pair in pairs:
+            triplets.append(list[i] + pair)
         
+    return triplets
+
+def pair_sum_sorted(list, start:int, target:int):
+    pairs = []
+    left, right = 0, len(list)-1
+    while left < right:
+        sum = list[left] + list[right]
+        if sum < target: 
+            left+=1
+        elif sum > target:
+            right -=1
+        else:
+            while left < right and list[left] == list[left-1]:
+                left +=1
+                pairs.append([list[left], list[right]])
+
+    return pairs
+
