9d281e839dcd785eef252b8bbf22e3ccf05df021
kateswim <goncharovakiev@gmail.com>
2025-07-14 22:41:12 -0700

    updates

--- Hash Maps Sets/verify_sudoku_board.py
+++ Hash Maps Sets/verify_sudoku_board.py
@@ -20,4 +20,33 @@ def verify_sudoku_board(board: list[list[int]]):
             
             row_sets[r].add(num)
             column_sets[c].add(num)
+            subgrid_sets[r // 3][c // 3].add(num)
 
+    return True
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+print(verify_sudoku_board(board))  # Should return True if the board is valid
+board_invalid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+board_invalid[0][0] = 6  # invalid number
+print(verify_sudoku_board(board_invalid)) # Should return False if the board is invalid
