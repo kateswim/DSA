ba450527d74af275bf970750f90980186bcd35e3
kateswim <goncharovakiev@gmail.com>
2025-07-14 21:43:58 -0700

    half

--- Hash Maps Sets/verify_sudoku_board.py
+++ Hash Maps Sets/verify_sudoku_board.py
@@ -2,3 +2,9 @@ def verify_sudoku_board(board: list[list[int]]):
     row_sets = [set() for _ in range(9)]
     column_set = [set() for _ in range(9)]
     subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
+
+    for r in range(9):
+        for c in range(9):
+            num = board[r][c]
+            if num == 0:
+                continue
