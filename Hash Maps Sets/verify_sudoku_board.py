7507c5972682b6e775d149607c8fd962e0df038b
kateswim <goncharovakiev@gmail.com>
2025-07-14 22:30:54 -0700

    updates

--- Hash Maps Sets/verify_sudoku_board.py
+++ Hash Maps Sets/verify_sudoku_board.py
@@ -1,6 +1,6 @@
 def verify_sudoku_board(board: list[list[int]]):
     row_sets = [set() for _ in range(9)]
-    column_set = [set() for _ in range(9)]
+    column_sets = [set() for _ in range(9)]
     subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
 
     for r in range(9):
@@ -8,3 +8,16 @@ def verify_sudoku_board(board: list[list[int]]):
             num = board[r][c]
             if num == 0:
                 continue
+
+            if num in row_sets[r]:
+                return False
+            
+            if num in column_sets[c]:
+                return False
+            
+            if num in subgrid_sets[r // 3][c // 3]:
+                return False
+            
+            row_sets[r].add(num)
+            column_sets[c].add(num)
+
