da10d284d4ca243b9183c75cd39c3e1d1b48006b
kateswim <goncharovakiev@gmail.com>
2025-07-15 22:29:03 -0700

    Update zero_stripping.py

--- Hash Maps Sets/zero_stripping.py
+++ Hash Maps Sets/zero_stripping.py
@@ -5,6 +5,7 @@ def zero_striping(matrix: list[list[int]]):
     m, n = len(matrix), len(matrix[0])
 
     first_row_has_zero = False
+    
     for c in range(n):
         if matrix[0][c] == 0:
             first_row_has_zero = True
