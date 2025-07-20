b162b2f4c413d86dece570647c1be80ccefaee2d
kateswim <goncharovakiev@gmail.com>
2025-07-19 22:26:55 -0700

    updates

--- Linked Lists/linked_list_reversal.py
+++ Linked Lists/linked_list_reversal.py
@@ -4,7 +4,6 @@ class ListNode:
         self.next = next      
 
 
-
 def linked_list_reversal(head: ListNode):
     current_node, previous_node = head, None
     while current_node:
@@ -15,6 +14,13 @@ def linked_list_reversal(head: ListNode):
 
     return previous_node
 
+def print_linked_list(head):
+    node = head
+    while node:
+        print(node.val, end=' ')
+        node = node.next
+    print()
+
 n1 = ListNode(1)
 n2 = ListNode(2)
 n3 = ListNode(3)
@@ -23,3 +29,6 @@ n1.next = n2
 n2.next = n3
 n3.next = n4
 
+head = n1
+reversed_head = linked_list_reversal(head)
+print_linked_list(reversed_head)
