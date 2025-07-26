7f56dd06253c09d4b9aeabc8a01c38042bf0ce4d
kateswim <goncharovakiev@gmail.com>
2025-07-25 19:21:47 -0700

    Update remove_kth_last_node.py

--- Linked Lists/remove_kth_last_node.py
+++ Linked Lists/remove_kth_last_node.py
@@ -21,3 +21,23 @@ def remove_kth_last_node(head: ListNode, k: int):
     trailer.next = trailer.next.next
     return dummy.next
 
+def print_linked_list(head):
+    node = head
+    while node:
+        print(node.val, end=' ')
+        node = node.next
+    print()
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+
+head = n1
+k = 2
+new_head = remove_kth_last_node(head, k)
+print_linked_list(new_head)
