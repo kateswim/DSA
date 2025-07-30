6e42d602890944c1c393ef83076244b1f34dc074
kateswim <goncharovakiev@gmail.com>
2025-07-29 22:03:54 -0700

    Create linked_list_intersection.py

---
+++ Linked Lists/linked_list_intersection.py
@@ -0,0 +1,15 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val       
+        self.next = next  
+
+
+def linked_list_intersection(head_A: ListNode, head_B: ListNode):
+    ptr_A, ptr_B = head_A, head_B
+
+    while ptr_A != ptr_B:
+        ptr_A = ptr_A.next if ptr_A else head_B
+
+        ptr_B = ptr_B.next if ptr_B else head_A
+
+    return ptr_A
