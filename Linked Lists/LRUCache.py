ff4b26b8254166650c480895ffc554b5fce0cfc9
kateswim <goncharovakiev@gmail.com>
2025-08-01 22:48:51 -0700

    Update LRUCache.py

--- Linked Lists/LRUCache.py
+++ Linked Lists/LRUCache.py
@@ -5,5 +5,36 @@ class DoublyLinkedListNode:
         self.next = self.prev = None
 
 
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hasmap = {}
+        self.head = DoublyLinkedListNode(-1, -1)
+        self.tail = DoublyLinkedListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-def add_to_tail
+    def add_to_tail(self, node: DoublyLinkedListNode):
+        prev_node = self.tail.prev
+        node.prev = prev_node
+        node.next = self.tail
+        prev_node.next = node
+        self.tail.prev = node
+
+    def remove_node(self, node: DoublyLinkedListNode):
+        node.prev.nexxt = node.next
+        node.next.prev = node.prev
+
+    def get(self, key: int):
+        if key not in self.hashmap:
+            return -1
+        
+        self.remove_node(self.hashmap[key])
+        self.add_to_tail(self.hashmap[key])
+        return self.hashmap[key].val
+
+    def put(self, key: int, value: int):
+        if key in self.hashmap:
+            self.remove_node(self.hashmap[key])
+
+        
