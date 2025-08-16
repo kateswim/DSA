8804aedd5e8f7f8abb863f15e136d15b07ff6301
kateswim <goncharovakiev@gmail.com>
2025-08-16 15:41:37 -0700

    up

--- Linked Lists/happy_number.py
+++ Linked Lists/happy_number.py
@@ -9,4 +9,18 @@ def happy_number(n: int):
         elif fast == slow:
             return False
         
-        
+def get_next_num(x: int): 
+    next_num = 0
+    
+    while x > 0:
+        digit = x % 10
+        x //= 10 # calculates the result and updates x with that value
+
+        next_num += digit ** 2
+
+    return next_num       
+
+
+print(happy_number(19))  # True
+print(happy_number(2))   # False
+print(happy_number(7))   # True
